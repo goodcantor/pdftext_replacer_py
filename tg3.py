fd5e4e63d0eec6c542e4ca935a162bdc2e486962
Daniil Apekalovskii <stariksperson@gmail.com>
2024-02-27 21:54:17 +0300

    adding id

--- tg3.py
+++ tg3.py
@@ -140,7 +140,7 @@ async def process_replacements(text_items, user, chat_id):
             logging.error("Ошибка при удалении файла: %s", e)
 
 
-ALLOWED_USER_IDS = [189684105, 36995430, 700326689, 1056198933]  # Замените этими значениями реальные ID пользователей
+ALLOWED_USER_IDS = [189684105, 36995430, 700326689, 1056198933, 284867763]  
 
 # Функция проверки пользователя
 def is_user_allowed(user_id):
